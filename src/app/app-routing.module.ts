ff40887c7af56fe4ccb02f723921eca65253bfdb
VladBrok <vlad.brok99@gmail.com>
2023-09-14 11:18:39 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { GraphPageComponent } from './graph-page/graph-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'graph', component: GraphPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { GraphPageComponent } from './graph-page/graph-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    AdminPageComponent,
+    GraphPageComponent
   ],
   imports: [
     BrowserModule,
