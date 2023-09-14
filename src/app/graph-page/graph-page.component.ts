3545334659a7dad2f65d09c9d4ffc266b54a7eaf
VladBrok <vlad.brok99@gmail.com>
2023-09-14 17:24:47 +0300

    fetch graph data

--- src/app/graph-page/graph-page.component.ts
+++ src/app/graph-page/graph-page.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment as env } from '../../environments/environment';
+import { finalize } from 'rxjs';
+import { Graph } from '../models/graph';
 
 @Component({
   selector: 'app-graph-page',
@@ -7,11 +11,34 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./graph-page.component.css'],
 })
 export class GraphPageComponent {
-  graphId: string = '';
+  assessmentId: string = '';
+  isLoading = false;
+  graphData: Graph | null = null;
+  loadingError: any = null;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.queryParams.subscribe((params) => {
-      this.graphId = params['id'];
+      this.assessmentId = params['id'];
+
+      if (!this.assessmentId) {
+        return;
+      }
+
+      this.http
+        .get<Graph>(`${env.apiUrl}/userassessments/graph`, {
+          params: new HttpParams().set('id', this.assessmentId),
+        })
+        .pipe(finalize(() => (this.isLoading = false)))
+        .subscribe({
+          next: (result) => {
+            this.graphData = result;
+            this.loadingError = null;
+          },
+          error: (e) => {
+            console.error(e);
+            this.loadingError = e;
+          },
+        });
     });
   }
 }
