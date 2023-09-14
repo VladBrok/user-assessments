fc5cbfb74cd902696e0143fe022308112e8b5182
VladBrok <vlad.brok99@gmail.com>
2023-09-14 13:05:29 +0300

    fix apiUrls, redirect to dashboard on auth

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -4,6 +4,7 @@ import { environment as env } from '../../environments/environment';
 import { FormBuilder, Validators } from '@angular/forms';
 import { LoginRequest } from '../../models/login-request';
 import { Subject, finalize } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,12 +14,13 @@ import { Subject, finalize } from 'rxjs';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   isLoading = new Subject<boolean>();
+  authError = new Subject<any>();
   loginForm = this.fb.group({
     email: [null, Validators.compose([Validators.required, Validators.email])],
     password: [null, Validators.required],
   });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit(): void {
     if (!this.loginForm.valid) {
@@ -36,8 +38,14 @@ export class LoginPageComponent {
       .post(`${env.apiUrl}/login`, body)
       .pipe(finalize(() => this.isLoading.next(false)))
       .subscribe({
-        next: () => console.log('next'),
-        error: (e) => console.log('error', e),
+        next: () => {
+          this.authError.next(null);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (e) => {
+          console.error(e);
+          this.authError.next(e);
+        },
       });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://user-assessment-api.vercel.app',
+  apiUrl: 'https://user-assessment-api.vercel.app/api',
 };
