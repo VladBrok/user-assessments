47aa3accee6939858880da6e7ae250962889c3a4
VladBrok <vlad.brok99@gmail.com>
2023-09-14 17:09:52 +0300

    get graphId from route

--- src/app/graph-page/graph-page.component.ts
+++ src/app/graph-page/graph-page.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-graph-page',
   templateUrl: './graph-page.component.html',
-  styleUrls: ['./graph-page.component.css']
+  styleUrls: ['./graph-page.component.css'],
 })
 export class GraphPageComponent {
+  graphId: string = '';
 
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      this.graphId = params['id'];
+    });
+  }
 }
