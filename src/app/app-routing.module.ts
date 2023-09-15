3fa81312256e033e0ddbc37ca78812fd7112ac49
VladBrok <vlad.brok99@gmail.com>
2023-09-15 12:53:14 +0300

    use one authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { DashboardPageComponent } from './components/dashboard-page/dashboard-page.component';
 import { AdminPageComponent } from './components/admin-page/admin-page.component';
 import { GraphPageComponent } from './components/graph-page/graph-page.component';
-import { loginPageGuard } from './guards/login-page.guard';
-import { adminGuard } from './guards/admin.guard';
 import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
@@ -17,14 +15,18 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
-    canActivate: [loginPageGuard],
+    canActivate: [authGuard],
   },
   {
     path: 'dashboard',
     component: DashboardPageComponent,
     canActivate: [authGuard],
   },
-  { path: 'admin', component: AdminPageComponent, canActivate: [adminGuard] },
+  {
+    path: 'admin',
+    component: AdminPageComponent,
+    canActivate: [authGuard],
+  },
   { path: 'graph', component: GraphPageComponent, canActivate: [authGuard] },
 ];
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,12 +2,23 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route) => {
   const router = inject(Router);
   const auth = inject(AuthService);
-  if (!auth.isLoggedIn()) {
+
+  if (auth.isLoggedIn() && route.routeConfig?.path === 'login') {
+    router.navigate(['/dashboard']);
+    return false;
+  }
+
+  if (
+    (!auth.isLoggedIn() ||
+      (route.routeConfig?.path === 'admin' && !auth.isAdmin())) &&
+    route.routeConfig?.path !== 'login'
+  ) {
     router.navigate(['/login']);
     return false;
   }
+
   return true;
 };
